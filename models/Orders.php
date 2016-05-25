ac30717ef4f52a285843c7f21a31397cd2f25a7e
dvaganov <d.vaganov92@yandex.ru>
2016-05-25 16:20:04 +0300

    Add table orders and create related model

---
+++ models/Orders.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "orders".
+ *
+ * @property integer $id
+ * @property string $name
+ * @property string $address
+ * @property string $email
+ * @property string $phone
+ * @property string $date_create
+ */
+class Orders extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'orders';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['id'], 'required'],
+            [['id'], 'integer'],
+            [['date_create'], 'safe'],
+            [['name', 'address', 'email'], 'string', 'max' => 255],
+            [['phone'], 'string', 'max' => 10],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'address' => 'Address',
+            'email' => 'Email',
+            'phone' => 'Phone',
+            'date_create' => 'Date Create',
+        ];
+    }
+}
